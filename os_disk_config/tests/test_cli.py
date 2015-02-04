6ce5caa1df87f75f1a5d10568de99636ba6c5d32
Ben Nemec <bnemec@redhat.com>
2015-02-04 17:58:27 +0000

    Unit test cleanup
    Remove some leftover references to os-net-config and some tests
    that were specific to it.

--- os_disk_config/tests/test_cli.py
+++ os_disk_config/tests/test_cli.py
@@ -17,17 +17,18 @@
 import os.path
 import sys
 
-from os_net_config import cli
-from os_net_config.tests import base
+from oslotest import base
 import six
+
+from os_disk_config import cli
 
 
 REALPATH = os.path.dirname(os.path.realpath(__file__))
 SAMPLE_BASE = os.path.join(REALPATH, '../../', 'etc',
-                           'os-net-config', 'samples')
+                           'os-disk-config', 'samples')
 
 
-class TestCli(base.TestCase):
+class TestCli(base.BaseTestCase):
 
     def run_cli(self, argstr, exitcodes=(0,)):
         orig = sys.stdout
@@ -45,70 +46,3 @@ class TestCli(base.TestCase):
         sys.stderr.close()
         sys.stderr = orig_stderr
         return (stdout, stderr)
-
-    def test_bond_noop_output(self):
-        bond_yaml = os.path.join(SAMPLE_BASE, 'bond.yaml')
-        bond_json = os.path.join(SAMPLE_BASE, 'bond.json')
-        stdout_yaml, stderr = self.run_cli('ARG0 --provider=ifcfg --noop '
-                                           '-c %s' % bond_yaml)
-        self.assertEqual('', stderr)
-        stdout_json, stderr = self.run_cli('ARG0 --provider=ifcfg --noop '
-                                           '-c %s' % bond_json)
-        self.assertEqual('', stderr)
-        sanity_devices = ['DEVICE=br-ctlplane',
-                          'DEVICE=em2',
-                          'DEVICE=em1',
-                          'DEVICE=bond1',
-                          'DEVICETYPE=ovs']
-        for dev in sanity_devices:
-            self.assertIn(dev, stdout_yaml)
-        self.assertEqual(stdout_yaml, stdout_json)
-
-    def test_bridge_noop_output(self):
-        bridge_yaml = os.path.join(SAMPLE_BASE, 'bridge_dhcp.yaml')
-        bridge_json = os.path.join(SAMPLE_BASE, 'bridge_dhcp.json')
-        stdout_yaml, stderr = self.run_cli('ARG0 --provider=eni --noop -c %s' %
-                                           bridge_yaml)
-        self.assertEqual('', stderr)
-        stdout_json, stderr = self.run_cli('ARG0 --provider=eni --noop -c %s' %
-                                           bridge_json)
-        self.assertEqual('', stderr)
-        sanity_devices = ['iface br-ctlplane inet dhcp',
-                          'iface em1',
-                          'ovs_type OVSBridge']
-        for dev in sanity_devices:
-            self.assertIn(dev, stdout_yaml)
-        self.assertEqual(stdout_yaml, stdout_json)
-
-    def test_vlan_noop_output(self):
-        vlan_yaml = os.path.join(SAMPLE_BASE, 'bridge_vlan.yaml')
-        vlan_json = os.path.join(SAMPLE_BASE, 'bridge_vlan.json')
-        stdout_yaml, stderr = self.run_cli('ARG0 --provider=ifcfg --noop -c %s'
-                                           % vlan_yaml)
-        self.assertEqual('', stderr)
-        stdout_json, stderr = self.run_cli('ARG0 --provider=ifcfg --noop -c %s'
-                                           % vlan_json)
-        self.assertEqual('', stderr)
-        sanity_devices = ['DEVICE=br-ctlplane',
-                          'DEVICE=em1',
-                          'DEVICE=vlan16',
-                          'DEVICETYPE=ovs']
-        for dev in sanity_devices:
-            self.assertIn(dev, stdout_yaml)
-        self.assertEqual(stdout_yaml, stdout_json)
-
-    def test_interface_noop_output(self):
-        interface_yaml = os.path.join(SAMPLE_BASE, 'interface.yaml')
-        interface_json = os.path.join(SAMPLE_BASE, 'interface.json')
-        stdout_yaml, stderr = self.run_cli('ARG0 --provider=ifcfg --noop -c %s'
-                                           % interface_yaml)
-        self.assertEqual('', stderr)
-        stdout_json, stderr = self.run_cli('ARG0 --provider=ifcfg --noop -c %s'
-                                           % interface_json)
-        self.assertEqual('', stderr)
-        sanity_devices = ['DEVICE=em1',
-                          'BOOTPROTO=static',
-                          'IPADDR=192.0.2.1']
-        for dev in sanity_devices:
-            self.assertIn(dev, stdout_yaml)
-        self.assertEqual(stdout_yaml, stdout_json)
